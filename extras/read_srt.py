7975f5af265c5c35efeb1202a58c481b0a7fd4dc
5war00p <57614947+5war00p@users.noreply.github.com>
2021-06-30 11:13:44 +0530

    Initial Commit

---
+++ extras/read_srt.py
@@ -0,0 +1,58 @@
+import re
+import nltk
+from level import get_level
+from dictionary import get_meaning
+
+fname = 'The Vampire Diaries - 1x02 - The Night of the Comet.HDTV.FQM.en.srt'
+
+def categorize(lis):
+
+    # params: list of words (List)
+    # return: meanings of the words (Dict)
+
+    words = re.findall(r'\w+', lis)
+    words = list(map(lambda x: x.lower(), words))
+    words = list(set(words))
+
+    definitions = {}
+    for word in words:
+        level = get_level(word)
+        if level[0] == 'Expert':
+            definition = get_meaning(word)
+            if definition is not None:
+                definitions[word] = definition
+    return definitions
+
+def get_meanings(fname):
+    with open(fname, 'r', encoding='utf-8') as ipfile:
+        Lines = ipfile.readlines()
+        Lines = list(map(lambda line: line.replace('\n', ''), Lines))
+        updated = []
+        pointer = 0
+        for index,str in enumerate(Lines):
+            each = {}
+            if str == '':
+                each['line_number'] = Lines[pointer]
+                each['time_interval'] = Lines[pointer+1]
+                each['messages'] = Lines[pointer+2: index]
+                updated.append(each)
+                pointer = index + 1
+
+        result = {}
+        for ln, line in enumerate(updated):
+            messages = line['messages']
+            message = ' '.join(messages)
+            meanings = categorize(message)
+            if meanings != {}:
+                result[ln] = meanings
+
+        # print(result)
+        return result
+            # for message in messages:
+                # message = re.sub(r'[^\w]', ' ', message)
+                #tokens = nltk.word_tokenize(message)
+                #pos_tagged_tokens = nltk.pos_tag(tokens)
+                #print(pos_tagged_tokens)
+                # print(message)
+
+get_meanings(fname)
---
+++ level.py
@@ -0,0 +1,31 @@
+# to convert character stream into python object
+import pickle
+
+# reading from Wikipedia pickle file contains character stream
+with open('word_db/enWikipediaDictTermCounts.pickle', 'rb') as file:
+    # load pickle file
+    d = pickle.load(file)
+
+
+def get_level(word):
+    # params: a word (string)
+    # return: difficulty level of the english word (string)
+
+    # {1: 'Beginner', 2: 'Basic', 3: 'Intermediate', 4: 'Expert'}
+
+    k = d.get(word, 0)
+
+    if 0 < k <= 500:
+        return 4, k
+    elif 500 < k <= 10000:
+        return 3, k
+    elif 10000 < k < 30000:
+        return 2, k
+    elif k > 30000:
+        return 1, k
+    else:
+        return None, 0
+
+# Test Drive Code
+
+# print(get_level("tremble"))
